9b86242f7848f7806a7f3a83e2389b6028e647b0
Audrey457 <laudrey457@gmail.com>
2023-10-09 12:24:28 +0200

    GetRegionById

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Controllers
+{
+    // https://localhost:portnumber/api/regions
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalksDbContext dbContext;
+
+        public RegionsController(NZWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        //GET : https://localhost:portnumber/api/regions
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = dbContext.Regions.ToList();
+            return Ok(regions);
+        }
+
+        // GET single region by id
+        // GET: https://localhost:portnumber/api/regions/{id}
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute]Guid id) 
+        {
+            var region = dbContext.Regions.FirstOrDefault(r => r.Id == id);
+            //Autre possibilité. Find prend la clé primaire uniquement.
+            //var region = dbContext.Regions.Find(id);
+            if(region  == null)
+            {
+                return NotFound();
+            }
+            return Ok(region);
+        }
+    }
+}
