f0c8ae18cdeeb561fccf47131cdffecb2cbf3986
Audrey457 <laudrey457@gmail.com>
2023-11-09 13:05:03 +0100

    Add walk

---
+++ NZWalks.API/Controllers/WalksController.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    // /api/walks
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WalksController : ControllerBase
+    {
+        private readonly IMapper mapper;
+        private readonly IWalkRepository walkRepository;
+
+        public WalksController(IMapper mapper, IWalkRepository walkRepository)
+        {
+            this.mapper = mapper;
+            this.walkRepository = walkRepository;
+        }
+
+        // Create walk
+        // POST : /api/walks
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] AddWalkRequestDto addWalkRequestDto)
+        {
+            // Map DTO to domain model
+            var walk = mapper.Map<Walk>(addWalkRequestDto);
+            await walkRepository.CreateAsync(walk);
+            //Map domain model to DTO
+            return Ok(mapper.Map<WalkDto>(walk));
+
+        }
+
+        //GET Walks
+        //GET: /api/walks
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var walks = await walkRepository.GetAllAsync();
+            //Map domain model to DTO
+            return Ok(mapper.Map<List<WalkDto>>(walks));
+        }
+    }
+}
