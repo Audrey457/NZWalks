d129d72b96d3c5467c66f6932f99f2f37fe7ab54
Audrey <laudrey457@gmail.com>
2023-11-27 11:55:13 +0100

    Authentication : ajout packages, creation db

---
+++ NZWalks.API/Data/NZWalksAuthDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksAuthDbContext : IdentityDbContext
+    {
+        public NZWalksAuthDbContext(DbContextOptions<NZWalksAuthDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var readerRoleId = "392dd4e1-a5f7-4db3-be23-9a2742bc76ed";
+            var writerRoleId = "122d0e53-c738-4732-99b3-261cbeae0837";
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = readerRoleId,
+                    ConcurrencyStamp = readerRoleId,
+                    Name="Reader",
+                    NormalizedName = "Reader".ToUpper()
+                },
+                new IdentityRole
+                {
+                    Id=writerRoleId,
+                    ConcurrencyStamp   =writerRoleId,
+                    Name="Writer",
+                    NormalizedName = "Writer".ToUpper()
+                }
+            };
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
+    }
+}
