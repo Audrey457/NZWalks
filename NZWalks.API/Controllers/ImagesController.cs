65450b917c83865bb18daeb7adc105057f9ca97f
Audrey <laudrey457@gmail.com>
2023-12-01 12:54:05 +0100

    Controller and action method for Image

---
+++ NZWalks.API/Controllers/ImagesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.DTO;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        // POST: /api/Images/Upload
+        [HttpPost]
+        [Route("Upload")]
+        public async Task<IActionResult> Upload([FromForm] ImageUploadRequestDto request)
+        {
+            ValidateFileUpload(request);
+            if(ModelState.IsValid)
+            {
+                //Upload image
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        private void ValidateFileUpload(ImageUploadRequestDto request)
+        {
+            var allowedExtension = new string[] { ".jpg", ".jpeg", ".png" };
+            if(!allowedExtension.Contains(Path.GetExtension(request.FileName))) 
+            {
+                ModelState.AddModelError("file", "Unsupported file extension");
+            }
+
+            if(request.File.Length > 10485760)
+            {
+                ModelState.AddModelError("file", "File more than 10MB, please upload a smaller size file.");
+            }
+        }
+    }
+}
